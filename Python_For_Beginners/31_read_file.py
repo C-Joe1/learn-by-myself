3f18487123e3ee294774f7846c175c64eb383cba
C_Joe <C.Joe0_1@outlook.com>
2023-10-21 03:46:21 +0800

    Python Updata.

---
+++ Python_For_Beginners/31_read_file.py
@@ -0,0 +1,16 @@
+# 打开文件，用 readlines & for loop 读取文件
+with open("./data.txt", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        print(line)
+
+# # 打开文件，用 readline & while loop 读取文件
+# with open("./data.txt", "r", encoding="utf-8") as f:
+#     line = f.readline()
+#     while line != "":
+#         print(line)
+#         line = f.readline()
+
+# # 打开文件，用 read 读取文件
+# with open ("./data.txt", "r", encoding="utf-8") as f:
+#     print(f.read())
---
+++ Python_For_Beginners/README.md
@@ -0,0 +1,5 @@
+# Python自学-个人纪录
+
+[【01-理论课】快速易懂Python入门_哔哩哔哩_bilibili](https://www.bilibili.com/video/BV1d54y1g7db/?p=2)
+
+目前学习Python爬虫中。
